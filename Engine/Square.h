3acfc1c88641527268ed688ebb072e5167c4366f
n <n@LENOVO-PC>
2017-01-01 18:01:00 +0700

    .

--- Engine/Square.h
+++ Engine/Square.h
@@ -50,9 +50,39 @@ public:
 	{
 		float massRatio = m_mass / other.m_mass;
 
+		// TODO: Use Minkowski difference
+		/*
+		http://www.wildbunny.co.uk/blog/2011/04/20/collision-detection-for-dummies/
+		*/
+
+		// TODO: GET CLOSEST FACE - 
+		/*
+		A supporting vertex is simply a vertex which is "most in the direction of" 
+		a given direction vector. Mathematically, this can be found as THE VERTEX 
+		WHICH HAS THE GREATEST DOT PRODUCT with a given direction vector.
+		*/
+		
+	//	if ( other.m_mass >= 200.f ) // Partner has INFINITE MASS
+	//  {	
+	//		// GET CLOSEST FACE 
+	//	Vec2 normal = { 0.f, 1.f};
+
+	//	XMFLOAT3 rbndComponent = normal * DotProduct( m_attributes.velocLin, normal );
+	//	forceOnThis = m_attributes.velocLin - rbndComponent * 3.2f;
+	//	forceOnPartner = { 0.f, 0.f, 0.f };
+	//}
+	//	else if ( m_attributes.mass >= 4000 ) // This object has INFINITE MASS
+	//	{
+	//		//XMFLOAT3 normal = m_AABBNorms[ eDir ];
+	//		XMFLOAT3 normal = { 0.f, 1.f, 0.f };
+
+	//		XMFLOAT3 rbndComponent = normal * DotProduct( partnerAttributes.velocLin, normal );
+	//		forceOnThis = { 0.f, 0.f, 0.f };
+	//		forceOnPartner = partnerAttributes.velocLin + rbndComponent;
+	//	}
+
 		// swap velocities
 		Vec2 tempVeloc = m_velocity;
-
 		m_velocity = other.m_velocity * massRatio;
 		other.m_velocity = tempVeloc / massRatio;
 	}
