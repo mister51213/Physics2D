9f2386cfc85a23dafcbdd34a34ee4654d477797d
n <n@LENOVO-PC>
2017-02-02 10:39:54 +0700

    changed resolution

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -94,6 +94,6 @@ private:
 	D3D11_MAPPED_SUBRESOURCE							mappedSysBufferTexture;
 	Surface												sysBuffer;
 public:
-	static constexpr unsigned int ScreenWidth = 640u;
-	static constexpr unsigned int ScreenHeight = 640u;
+	static constexpr unsigned int ScreenWidth = 800u;
+	static constexpr unsigned int ScreenHeight = 800u;
 };
