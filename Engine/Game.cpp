b48d77e0d19dd9be8f0e59919eb9df06a05894de
n <n@LENOVO-PC>
2017-02-17 18:55:58 +0700

    serious drawing issues

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -34,27 +34,9 @@ Game::Game( MainWindow& wnd )
 	gfx( wnd )
 {
 	/*************SET MASS TO 0 FOR IMMOBILE OBJECT**************/
-	//// colliding squares
-	//m_squares.push_back( Square( .1f, { -.5f, 0.f }, 2.f, 0.5f ) );
-	//m_squares.push_back( Square( .1f, { .5f, 0.f }, 1.f, 0.5f ) );
-
-	//// walls
-	//m_squares.push_back( Square( 1.45f, { -1.5f, 0.f }, 0.f, 1.0f ) );
-	//m_squares.push_back( Square( 1.45f, { 1.5f, 0.f }, 0.f, 1.0f ) );
-
-	//// floor
-	//m_squares.push_back( Square( 1.45f, { 0.f, -1.5f }, 0.f, 1.0f ) );
-
-	//// ceiling
-	//m_squares.push_back( Square( 1.45f, { 0.f, 1.5f }, 0.f, 1.0f ) );
-
-	/*************SET MASS TO 0 FOR IMMOBILE OBJECT**************/
 	// colliding squares
-	// TODO: specify shape enum in constructor of body
-	//m_bodies.emplace_back( new Body( .1f, { -.5f, 0.f }, 2.f, 0.5f, Shape::SQUARE ) );
-	//m_bodies.emplace_back( new Body( .1f, { .5f, 0.f }, 1.f, 0.5f, Shape::SQUARE ) );
-	m_bodies.emplace_back( new Body( .5f, { -.5f, 0.f }, 2.f, 0.5f, Shape::CIRCLE ) );
-	m_bodies.emplace_back( new Body( .5f, { .5f, 0.f }, 1.f, 0.5f, Shape::CIRCLE ) );
+	m_bodies.emplace_back( new Body( .1f, { -.5f, 0.f }, 2.f, 0.5f, Shape::CIRCLE ) );
+	m_bodies.emplace_back( new Body( .1f, { .5f, 0.f }, 1.f, 0.5f, Shape::CIRCLE ) );
 
 	// walls
 	m_bodies.emplace_back( new Body( 1.45f, { -1.5f, 0.f }, 0.f, 1.0f, Shape::SQUARE ) );
@@ -77,27 +59,12 @@ void Game::Go()
 
 void Game::DoCollision()
 {
-	//for ( int i = 0; i < m_squares.size() - 1; ++i )
-	//{
-	//	for ( int j = i + 1; j < m_squares.size(); ++j )
-	//	{
-	//		Vec2 normal;
-	//		float penetration;
-	//		if ( AABBvAABB( m_squares[ i ], m_squares[ j ], normal, penetration ) )
-	//		{
-	//			ResolveCollision( m_squares[ i ], m_squares[ j ], normal );			
-	//			CorrectPosition( m_squares[ i ], m_squares[ j ] , normal, penetration);			
-	//		}
-	//	}
-	//}
 	for ( int i = 0; i < m_bodies.size() - 1; ++i )
 	{
 		for ( int j = i + 1; j < m_bodies.size(); ++j )
 		{
 			Vec2 normal;
 			float penetration;
-			// TODO: use pointer to function dispatch array
-//			if ( AABBvAABB_temp( *m_bodies[ i ], *m_bodies[ j ], normal, penetration ) )
 			if ( CollisionArray[ m_bodies[ i ]->m_pShape->GetType() ][ m_bodies[ j ]->m_pShape->GetType() ]( *m_bodies[ i ], *m_bodies[ j ], normal, penetration ) )
 			{
 				ResolveCollision_temp( *m_bodies[ i ], *m_bodies[ j ], normal );			
@@ -129,42 +96,6 @@ void Game::UpdateModel()
 	//	m_squares[ i ].UpdatePosition(dTime);
 		m_bodies[ i ]->UpdatePosition(dTime);
 	}
-	
-		//// MOVE LEFT SQUARE
-		//if ( wnd.kbd.KeyIsPressed( 'W' ) )
-		//{
-		//	m_squares[ 0 ].Thrust( {0.f, 0.3f}, dTime );
-		//}
-		//if ( wnd.kbd.KeyIsPressed( 'S' ) )
-		//{
-		//	m_squares[ 0 ].Thrust( {0.f, -0.3f}, dTime );
-		//}
-		//if ( wnd.kbd.KeyIsPressed( 'A' ) )
-		//{
-		//	m_squares[ 0 ].Thrust( {-0.4, 0.f}, dTime );
-		//}
-		//if ( wnd.kbd.KeyIsPressed( 'D' ) )
-		//{
-		//	m_squares[ 0 ].Thrust( {0.4, 0.f}, dTime );
-		//}
-
-		//// MOVE RIGHT SQUARE
-		//if ( wnd.kbd.KeyIsPressed( VK_UP ) )
-		//{
-		//	m_squares[ 1 ].Thrust( { 0.f, 0.3f }, dTime );
-		//}
-		//if ( wnd.kbd.KeyIsPressed( VK_DOWN ) )
-		//{
-		//	m_squares[ 1 ].Thrust( { 0.f, -0.3f }, dTime );
-		//}
-		//if ( wnd.kbd.KeyIsPressed( VK_LEFT ) )
-		//{
-		//	m_squares[ 1 ].Thrust( {-0.4, 0.f}, dTime );
-		//}
-		//if ( wnd.kbd.KeyIsPressed( VK_RIGHT ) )
-		//{
-		//	m_squares[ 1 ].Thrust( {0.4, 0.f}, dTime );
-		//}
 	
 		// MOVE LEFT SQUARE
 		if ( wnd.kbd.KeyIsPressed( 'W' ) )
@@ -255,43 +186,42 @@ void Game::DrawVertices()
 
 void Game::ComposeFrame()
 {
-	vector<IndexedLineList> lines( nObjects );
-	for ( int i = 0; i < nObjects; i++)
-	{
-		lines[ i ] = gameObjects[ i ].GetLines();
-	}
+	//vector<IndexedLineList> lines( nObjects );
+	//for ( int i = 0; i < nObjects; i++)
+	//{
+	//	lines[ i ] = gameObjects[ i ].GetLines();
+	//}
 
-	// Rotate each Vertex
-	for ( int i = 0; i < nObjects; i++ )
-	{
-		const Mat3 rot =
-			Mat3::RotationX( gameObjects[ i ].theta_x ) *
-			Mat3::RotationY( gameObjects[ i ].theta_y ) *
-			Mat3::RotationZ( gameObjects[ i ].theta_z );
-		for ( Vec3& v : lines[ i ].vertices )
-		{
-			v *= rot;
-			v += { 0.0f, 0.0f, 1.0f };
-			sTransformer.Transform( v );
-		}
-	}
+	//// Rotate each Vertex
+	//for ( int i = 0; i < nObjects; i++ )
+	//{
+	//	const Mat3 rot =
+	//		Mat3::RotationX( gameObjects[ i ].theta_x ) *
+	//		Mat3::RotationY( gameObjects[ i ].theta_y ) *
+	//		Mat3::RotationZ( gameObjects[ i ].theta_z );
+	//	for ( Vec3& v : lines[ i ].vertices )
+	//	{
+	//		v *= rot;
+	//		v += { 0.0f, 0.0f, 1.0f };
+	//		sTransformer.Transform( v );
+	//	}
+	//}
 
-	// Connect the vertices with lines here to form cube
-	for ( int ind = 0; ind < nObjects; ind++ )
-	{
-		IndexedLineList linesLocal = lines[ ind ]; // TODO: shouldnt be copying them
-		for ( auto i = linesLocal.indices.cbegin(),
-			  end = linesLocal.indices.cend();
+	//// Connect the vertices with lines here to form cube
+	//for ( int ind = 0; ind < nObjects; ind++ )
+	//{
+	//	IndexedLineList linesLocal = lines[ ind ]; // TODO: shouldnt be copying them
+	//	for ( auto i = linesLocal.indices.cbegin(),
+	//		  end = linesLocal.indices.cend();
 
-			  i != end; std::advance( i, 2 ) )
-		{
-			gfx.DrawLine( linesLocal.vertices[ *i ], linesLocal.vertices[ *std::next( i ) ], Colors::Blue );
-		}
-	}
+	//		  i != end; std::advance( i, 2 ) )
+	//	{
+	//		gfx.DrawLine( linesLocal.vertices[ *i ], linesLocal.vertices[ *std::next( i ) ], Colors::Blue );
+	//	}
+	//}
 
 
-
-//	DrawVertices();
+	//DrawVertices();
 	sTransformer.Transform( m_bodies[ 0 ]->m_position );
 	sTransformer.Transform( m_bodies[ 1 ]->m_position );
 
